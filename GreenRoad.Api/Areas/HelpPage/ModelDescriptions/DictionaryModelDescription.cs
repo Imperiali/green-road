301f9c33a2d8ac96099207bfa26cdd631d60bf1d
Igor Imperiali <igor.imperiali@hotmail.com>
2018-02-09 08:40:43 -0200

    Projeto Api
    Related Work Items: #7

---
+++ GreenRoad.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GreenRoad.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ GreenRoad.Api/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GreenRoad.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
